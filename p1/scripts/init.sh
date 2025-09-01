7d08cf06009f6db3691923c315647e72f83a4358
mcordes <mcordes@vbox.myguest.virtualbox.org>
2025-09-01 20:30:22 +0200

    P1 | Reste a faire les commentaire dans la conf | En cours

--- p1/scripts/init.sh
+++ p1/scripts/init.sh
@@ -3,25 +3,26 @@
 ############################## Docker installation ##############################
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get -y install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 # Install the latest version
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo usermod -aG docker $USER
 
 ############################## GNS3 installation ################################
 sudo apt-get update
 
-sudo apt-get -y install python3 python3-pip pipx python3-pyqt5 python3-pyqt5.qtwebsockets python3-pyqt5.qtsvg qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst software-properties-common ca-certificates curl gnupg2 
+sudo apt-get install -y python3 python3-pip pipx python3-pyqt5 python3-pyqt5.qtwebsockets python3-pyqt5.qtsvg qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst software-properties-common ca-certificates curl gnupg2 
 
 wget http://ftp.fr.debian.org/debian/pool/non-free/d/dynamips/dynamips_0.2.14-1_amd64.deb
 sudo dpkg -i dynamips_0.2.14-1_amd64.deb
@@ -38,7 +39,7 @@ cd ../..
 rm -rf v0.8.3.tar.gz vpcs-0.8.3
 
 ############################## ubridge installation #############################
-sudo apt install -y cmake make g++ libpcap-dev libelf-dev libcap-dev
+sudo apt-get install -y cmake make g++ libpcap-dev libelf-dev libcap-dev
 git clone https://github.com/GNS3/ubridge.git
 cd ubridge
 make
