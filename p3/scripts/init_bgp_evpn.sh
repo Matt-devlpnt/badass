f6da2b93316a1e05e4b81dc71de0231ff8a3dd33
Matt-devlpnt <matthcordes123@gmail.com>
2025-12-17 11:25:47 +0000

    Final version, but often to test

--- p3/scripts/init_bgp_evpn.sh
+++ p3/scripts/init_bgp_evpn.sh
@@ -35,9 +35,9 @@ init_host3() {
 }
 
 
-#################################################################################
-# INIT LEAF1 ####################################################################
-#################################################################################
+##############
+# INIT LEAF1 #
+##############
 
 # ip link add br0 type bridge
 # Creates a network bridge device named br0. A bridge connects multiple network interfaces at the data link layer (Layer 2)
@@ -73,12 +73,38 @@ init_leaf1() {
 		brctl addif br0 eth1 ;
 		brctl addif br0 vxlan10
 	"
+
+	docker exec $1 vtysh -c "
+		configure terminal
+		!
+		hostname leaf_alpine_frrouting_mcordes-1
+		no ipv6 forwarding
+		!
+		interface eth0
+		 ip address 10.1.1.2/30
+		 ip ospf area 0
+		!
+		interface lo
+		 ip address 1.1.1.2/32
+		 ip ospf area 0
+		!
+		router bgp 1
+		 neighbor 1.1.1.1 remote-as 1
+		 neighbor 1.1.1.1 update-source lo
+		 !
+		 address-family l2vpn evpn
+		  neighbor 1.1.1.1 activate
+		  advertise-all-vni
+		 exit-address-family
+		!
+		router ospf
+	"
 }
 
 
-#################################################################################
-# INIT LEAF2 ####################################################################
-#################################################################################
+##############
+# INIT LEAF2 #
+##############
 
 # ip link add br0 type bridge
 # Creates a network bridge device named br0. A bridge connects multiple network interfaces at the data link layer (Layer 2)
@@ -109,18 +135,43 @@ init_leaf2() {
 	docker exec $1 bash -c "
 		ip link add br0 type bridge ;
 		ip link set dev br0 up ;
-		ip addr add 10.1.1.2/24 dev eth0 ;
-		ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789 ;
+		ip link add name vxlan10 type vxlan id 10 dstport 4789 ;
 		ip link set dev vxlan10 up ;
-		brctl addif br0 eth1 ;
+		brctl addif br0 eth0 ;
 		brctl addif br0 vxlan10
+	"
+
+	docker exec $1 vtysh -c "
+		configure terminal
+		!
+		hostname leaf_alpine_frrouting_mcordes-2
+		no ipv6 forwarding
+		!
+		interface eth1
+		 ip address 10.1.1.6/30
+		 ip ospf area 0
+		!
+		interface lo
+		 ip address 1.1.1.3/32
+		 ip ospf area 0
+		!
+		router bgp 1
+		 neighbor 1.1.1.1 remote-as 1
+		 neighbor 1.1.1.1 update-source lo
+		 !
+		 address-family l2vpn evpn
+		  neighbor 1.1.1.1 activate
+		  advertise-all-vni
+		 exit-address-family
+		!
+		router ospf
 	"
 }
 
 
-#################################################################################
-# INIT LEAF3 ####################################################################
-#################################################################################
+##############
+# INIT LEAF3 #
+##############
 
 # ip link add br0 type bridge
 # Creates a network bridge device named br0. A bridge connects multiple network interfaces at the data link layer (Layer 2)
@@ -151,20 +202,79 @@ init_leaf3() {
 	docker exec $1 bash -c "
 		ip link add br0 type bridge ;
 		ip link set dev br0 up ;
-		ip addr add 10.1.1.3/24 dev eth0 ;
-		ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789 ;
+		ip link add name vxlan10 type vxlan id 10 dstport 4789 ;
 		ip link set dev vxlan10 up ;
-		brctl addif br0 eth1 ;
+		brctl addif br0 eth0 ;
 		brctl addif br0 vxlan10
+	"
+
+	docker exec $1 vtysh -c "
+		configure terminal
+		!
+		hostname leaf_alpine_frrouting_mcordes-3
+		no ipv6 forwarding
+		!
+		interface eth2
+		 ip address 10.1.1.10/30
+		 ip ospf area 0
+		!
+		interface lo
+		 ip address 1.1.1.4/32
+		 ip ospf area 0
+		!
+		router bgp 1
+		 neighbor 1.1.1.1 remote-as 1
+		 neighbor 1.1.1.1 update-source lo
+		 !
+		 address-family l2vpn evpn
+		  neighbor 1.1.1.1 activate
+		  advertise-all-vni
+		 exit-address-family
+		!
+		router ospf
 	"
 }
 
 
-#################################################################################
-# INIT RR #######################################################################
-#################################################################################
+###########
+# INIT RR #
+###########
 
 init_rr() {
+	docker exec $1 vtysh -c "
+		configure terminal
+		!
+		hostname rr_mcordes
+		no ipv6 forwarding
+		!
+		interface eth0
+		 ip address 10.1.1.1/30
+		!
+		interface eth1
+		 ip address 10.1.1.5/30
+		!
+		interface eth2
+		 ip address 10.1.1.9/30
+		!
+		interface lo
+		 ip address 1.1.1.1/32
+		!
+		router bgp 1
+		 neighbor ibgp peer-group
+		 neighbor ibgp remote-as 1
+		 neighbor ibgp update-source lo
+		 bgp listen range 1.1.1.0/29 peer-group ibgp
+		 !
+		 address-family l2vpn evpn
+		  neighbor ibgp activate
+		  neighbor ibgp route-reflector-client
+		 exit-address-family
+		!
+		router ospf
+		 network 0.0.0.0/0 area 0
+		!
+		line vty 
+	"
 }
 
 
