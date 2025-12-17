376f9bfb9b25c7e472f95339888641bdd7930af9
Matt-devlpnt <matthcordes123@gmail.com>
2025-12-17 14:40:12 +0000

    Final version | just comment vtysh part of P3

--- p3/scripts/init_bgp_evpn.sh
+++ p3/scripts/init_bgp_evpn.sh
@@ -3,32 +3,36 @@
 DOCKER=$(docker ps -q)
 
 
-#################################################################################
-# INIT HOST1 ####################################################################
-#################################################################################
+##############
+# INIT HOST1 #
+##############
 
 # ip addr add 30.1.1.1/24 dev eth1
-# Assigns the IP address 30.1.1.1 with a subnet mask of /24 (255.255.255.0) to the eth1 interface. This configures eth0 for communication on the 10.1.1.0/24 network
+# Assigns the IP address 30.1.1.1 with a subnet mask of /24 (255.255.255.0) to the eth1 interface. This configures eth1 for communication on the 10.1.1.0/24 network
 
 init_host1() {
 	docker exec $1 ip addr add 30.1.1.1/24 dev eth1
 }
 
 
-#################################################################################
-# INIT HOST2 ####################################################################
-#################################################################################
+##############
+# INIT HOST2 #
+##############
 
-# ip addr add 30.1.1.2/24 dev eth1
-# Assigns the IP address 30.1.1.2 with a subnet mask of /24 (255.255.255.0) to the eth1 interface. This configures eth0 for communication on the 10.1.1.0/24 network
+# ip addr add 30.1.1.2/24 dev eth0
+# Assigns the IP address 30.1.1.2 with a subnet mask of /24 (255.255.255.0) to the eth0 interface. This configures eth0 for communication on the 10.1.1.0/24 network
 
 init_host2() {
 	docker exec $1 ip addr add 30.1.1.2/24 dev eth0
 }
 
 
-# ip addr add 30.1.1.2/24 dev eth1
-# Assigns the IP address 30.1.1.2 with a subnet mask of /24 (255.255.255.0) to the eth1 interface. This configures eth0 for communication on the 10.1.1.0/24 network
+##############
+# INIT HOST3 #
+##############
+
+# ip addr add 30.1.1.3/24 dev eth0
+# Assigns the IP address 30.1.1.3 with a subnet mask of /24 (255.255.255.0) to the eth0 interface. This configures eth0 for communication on the 10.1.1.0/24 network
 
 init_host3() {
 	docker exec $1 ip addr add 30.1.1.3/24 dev eth0
@@ -45,14 +49,9 @@ init_host3() {
 # ip link set dev br0 up
 # Activates the bridge device br0, bringing it online
 
-# ip addr add 10.1.1.1/24 dev eth0
-# Assigns the IP address 10.1.1.1 with a subnet mask of /24 (255.255.255.0) to the eth0 interface. This configures eth0 for communication on the 10.1.1.0/24 network
-
-# ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+# ip link add name vxlan10 type vxlan id 10 dstport 4789 ;
 # Creates a VXLAN interface named vxlan10 with:
 # VXLAN Network Identifier (VNI) 10.
-# Bound to the eth0 interface.
-# Multicast group address 239.1.1.1 for VXLAN traffic (used instead of a specific remote IP for multicast-based VXLAN).
 # Destination UDP port 4789 (standard VXLAN port)
 
 # ip link set dev vxlan10 up
@@ -112,14 +111,9 @@ init_leaf1() {
 # ip link set dev br0 up
 # Activates the bridge device br0, bringing it online
 
-# ip addr add 10.1.1.2/24 dev eth0
-# Assigns the IP address 10.1.1.2 with a subnet mask of /24 (255.255.255.0) to the eth0 interface. This configures eth0 for communication on the 10.1.1.0/24 network
-
-# ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+# ip link add name vxlan10 type vxlan id 10 dstport 4789 ;
 # Creates a VXLAN interface named vxlan10 with:
 # VXLAN Network Identifier (VNI) 10.
-# Bound to the eth0 interface.
-# Multicast group address 239.1.1.1 for VXLAN traffic (used instead of a specific remote IP for multicast-based VXLAN).
 # Destination UDP port 4789 (standard VXLAN port)
 
 # ip link set dev vxlan10 up
@@ -179,14 +173,9 @@ init_leaf2() {
 # ip link set dev br0 up
 # Activates the bridge device br0, bringing it online
 
-# ip addr add 10.1.1.2/24 dev eth0
-# Assigns the IP address 10.1.1.2 with a subnet mask of /24 (255.255.255.0) to the eth0 interface. This configures eth0 for communication on the 10.1.1.0/24 network
-
-# ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+# ip link add name vxlan10 type vxlan id 10 dstport 4789 ;
 # Creates a VXLAN interface named vxlan10 with:
 # VXLAN Network Identifier (VNI) 10.
-# Bound to the eth0 interface.
-# Multicast group address 239.1.1.1 for VXLAN traffic (used instead of a specific remote IP for multicast-based VXLAN).
 # Destination UDP port 4789 (standard VXLAN port)
 
 # ip link set dev vxlan10 up
